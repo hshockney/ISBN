51cd6a5a90df5b6afc7bed051261181f54cd3fb1
hshockney <shockney@atlanticbb.net>
2015-09-21 15:40:20 -0400

    added test for removing spaces from isbn

--- isbn_kata_test.rb
+++ isbn_kata_test.rb
@@ -14,11 +14,13 @@ assert_equal(true,length_verifier("1234567891234"))
 assert_equal(false,length_verifier("152347689"))
 end
 
-def test_for_removing_dashes
+def test_for_removing_dashes_from_isbn
 assert_equal("1234567891",remove_dashes_from_isbn("123-456-789-1"))
 end
 
-
+def test_for_removing_spaces_from_isbn
+assert_equal("9876543219",remove_spaces_from_isbn("987 654 321 9"))
+end
 
 
 
