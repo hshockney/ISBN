143c7267dbce078dcf7895f90576509642341f02
hshockney <shockney@atlanticbb.net>
2015-09-21 13:21:03 -0400

    changed test format to one line assertions, added two that are false

--- isbn_kata_test.rb
+++ isbn_kata_test.rb
@@ -4,14 +4,15 @@ require"minitest/autorun"
 class Isbn_verify<Minitest::Test  
 
 def test_for_ten_digits
-results=isbn_checker("1234567892")
-assert_equal(true,results)
+assert_equal(true,isbn_checker("1234567892"))
+assert_equal(false,isbn_checker("123456789234"))
 end
 
 def test_for_thirteen_digits
-results=isbn_checker("1234567891234")
-assert_equal(true,results)
+assert_equal(true,isbn_checker("1234567891234"))
+assert_equal(false,isbn_checker("152347689"))
 end
+
 
 
 
