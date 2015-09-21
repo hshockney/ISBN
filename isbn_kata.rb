70a25114492cb76b1160e2b083dab45a34e692f2
hshockney <shockney@atlanticbb.net>
2015-09-21 15:35:58 -0400

    changed the way code was being done to include functions for each step

--- isbn_kata.rb
+++ isbn_kata.rb
@@ -1,10 +1,27 @@
 def isbn_checker(book_number)
+  remove_spaces_from_isbn
+  remove_dashes_from_isbn
+  length_verifier
+end
+
+def length_verifier(book_number)
   if book_number.length==10
     true
   elsif book_number.length==13
     true
   else
     false
-
   end
-end
+  end
+
+def remove_dashes_from_isbn(book_number)
+  if book_number.include?"-"
+	book_number.delete!"-"
+  end
+  end
+
+def remove_spaces_from_isbn(book_number)
+  if book_number.include?" "
+     book_number.delete!" "
+  end
+  end
--- isbn_kata_test.rb
+++ isbn_kata_test.rb
@@ -4,13 +4,18 @@ require"minitest/autorun"
 class Isbn_verify<Minitest::Test  
 
 def test_for_ten_digits
-assert_equal(true,isbn_checker("1234567892"))
-assert_equal(false,isbn_checker("123456789234"))
+assert_equal(true,length_verifier("1234567892"))
+assert_equal(false,length_verifier("123456789234"))
+assert_equal(true,length_verifier("123 456 789 1"))
 end
 
 def test_for_thirteen_digits
-assert_equal(true,isbn_checker("1234567891234"))
-assert_equal(false,isbn_checker("152347689"))
+assert_equal(true,length_verifier("1234567891234"))
+assert_equal(false,length_verifier("152347689"))
+end
+
+def test_for_removing_dashes
+assert_equal("1234567891",remove_dashes_from_isbn("123-456-789-1"))
 end
 
 
