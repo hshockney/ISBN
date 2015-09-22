968e866de8c0280b6423b483a5dc56ff80f791c6
hshockney <shockney@atlanticbb.net>
2015-09-22 14:56:44 -0400

    adding changes now failures instead of errors

--- isbn_kata.rb
+++ isbn_kata.rb
@@ -1,13 +1,18 @@
-def isbn_checker
-  book_number_array(book_number)
+def isbn_checker(book_number)
+ book_number
+  book_number_array
   remove_spaces_from_isbn(book_number_array)
   remove_dashes_from_isbn(book_number_array)
   length_verifier(book_number_array)
   multiply_each_digit_by_its_position_if_10_digits(book_number_array)
 end
 
-def book_number_array(book_number)
-  book_number_array()==book_number.split(//)
+def book_number
+  book_number= book_number.to_s
+end
+
+def book_number_array
+  book_number_array=book_number.split(//)
   []<<book_number_array
 end
  
@@ -39,13 +44,14 @@ def remove_spaces_from_isbn(book_number_array)
   end
   
  def multiply_each_digit_by_its_position_if_10_digits(book_number_array)
+   sum=0
    while book_number_array.length==10
    book_number_array.each do |value,index_position|
-   sum = value*(index_position+1)
+   sum += value*(index_position+1)
    until index_position==[9]
-   book_number_array=sum %11
+   book_number_array=sum % 11
  if 
-  [9]==sum%11
+  book_number_array[9]==sum % 11
   true
  else
   false 
