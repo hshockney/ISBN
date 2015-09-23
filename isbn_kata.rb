9e60c330f92b69d5ca02c1b380c6a392511b9f2e
hshockney <shockney@atlanticbb.net>
2015-09-23 15:10:30 -0400

    updates to code add math for thriteen digits, still two failures

--- isbn_kata.rb
+++ isbn_kata.rb
@@ -3,7 +3,8 @@
    no_dashes_or_spaces_book_number= remove_dashes_from_isbn(no_spaces_book_number) 
    if length_verifier(no_dashes_or_spaces_book_number)==true
     isbn_array=book_number_array(no_dashes_or_spaces_book_number)
-	check_digit_valid_10(isbn_array)
+	check_digit_valid_10_math(isbn_array)
+	check_digit_valid_13_math(isbn_array)
 	else
 	false
   end
@@ -45,24 +46,59 @@ def remove_dashes_from_isbn(book_number_array)
   
   
 
-  def check_digit_valid_10(isbn_array)
-    new_array=[]
-	isbn_array.each {|value|
-	new_array<< value.to_i}
-	
+  def check_digit_valid_10_math(isbn_array)
+    # new_array=[]
+	# book_number_array.each {|value|
+	# new_array<< value.to_i}
 	
 	sum=0
-     new_array.each.with_index do|value,index|
-	 break if index ==9
-	 sum += (value*(index+1))
-	 
-	 check_digit= sum%11
-	 if check_digit==new_array[9]
+	if isbn_array.length==10
+	  isbn_array.each.with_index do|value,index_position|
+	break if index_position == 9
+	  sum += value*(index_position+1)
+	 end
+	 end
+	 check_digit= sum % 11
+	 if check_digit == isbn_array[9]
 	 true
+	 elsif
+	 check_digit==10
+	 "X"
 	 else
 	 false
-	 end
-	 end
+	 
  end
-  
+ end
+ def check_digit_valid_13_math(isbn_array) 
+   new_array=[]
+   new_array.each{ |value|
+   new_array<<value.to_i}
+ 
+ sum=0
+ check_digit=0
+ 
+ new_array.each_with_index do |value,index|
+ break if index==12
+       if index  %2==0
+	   sum+= value * 1
+	   else
+	   sum += value * 3
+	         end
+			 end
+	sum= sum %10
+	check_digit=(10-sum)
+	
+	  if check_digit==10
+	  check_digit=0
+	  end
+	  new_array[12]== check_digit
+end
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  
--- isbn_kata_test.rb
+++ isbn_kata_test.rb
@@ -22,13 +22,19 @@ end
  assert_equal("9876543219",remove_spaces_from_isbn("987 654 321 9"))
  end
 
-   def test_for_digits_of_10_formula
-   results=isbn_checker("0471958697")
-   assert_equal(true,results)
-   end
-
+ def test_for_digits_of_10_formula
+  assert_equal(false,isbn_checker(["0321146530"]))
+  assert_equal(true,isbn_checker(["047 195 8697"])) 
+	
+end
+  
 def test_string_convert_to_array
 assert_equal(["1","2","3","3","2","1","4","5","6","7"],book_number_array("1233214567"))
+end
+
+def test_for_thirteen_digit_math
+assert_equal(true,isbn_checker(["978-0-13-149505-0"]))
+assert_equal(false,isbn_checker(["4780470059029"]))
 end
 
 
